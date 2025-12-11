014fec34026d9b508cd0737ea94a1a13a67fd1e6
tarek.aufderstrasse@nexergroup.com <Tarek.AufderStrasse@nexergroup.com>
2025-12-11 22:29:23 +0100

    Changed to dvh

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex flex-column overflow-y-hidden" style="height: 100%; max-height: 100vh;">
+  <div class="d-flex flex-column overflow-y-hidden" style="height: 100%; max-height: 100dvh;">
     <router-view />
   </div>
 </template>
